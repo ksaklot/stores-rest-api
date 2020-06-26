b7f7117f2b67e2fbfb1f4f5c9143b0fba1b3f8b5
ksaklot <66646609+ksaklot@users.noreply.github.com>
2020-06-26 12:42:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 from security import authenticate,identity
-from db import db
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store,StoreList
@@ -13,10 +12,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite://
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
 app.secret_key = 'jose'
 api= Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
